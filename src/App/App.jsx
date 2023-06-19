7937d367b7d276cbe07263c86b2a54417144316d
machimozor <nabhaneibrahim77@gmail.com>
2023-06-19 17:19:51 +0200

    use state

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -3,21 +3,29 @@ import style from "./App.module.css";
 import Button from "./component/ui/Button";
 
 class App extends React.PureComponent {
-  counter = 0;
+  
   constructor(props) {
     super(props);
-    console.log("constructeur de App");
+    
+    this.state={counter:0}
   }
   render() {
     return (
       <div className={style.App}>
         valeur de counter :{" "}
         <span style={{ fontWeight: "900", fontSize: "42pt" }}>
-          {this.counter}
+          {this.state.counter}
         </span>
         <hr />
-        <Button bgcolor="tomato">-1</Button>
-        <Button bgcolor="skyblue">+1</Button>
+        <Button bgcolor="tomato" onButtonClick={()=>
+        { this.setState({counter:this.state.counter-1});
+        console.log(this.state.counter);
+        }
+        }>-1</Button>
+        <Button bgcolor="skyblue" onButtonClick={()=>
+        { this.setState({counter:this.state.counter+1});
+        console.log(this.state.counter);
+        }} >+1</Button>
       </div>
     );
   }
