da367008b2ea8a48d204a5632065428911b239a3
machimozor <nabhaneibrahim77@gmail.com>
2023-06-19 16:13:53 +0200

    nouveau commit

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -8,7 +8,16 @@ const App = (props) =>
   
   return (
     <div className="App">
-      <Button text="blaabbd" />
+    
+    <Button onButtonClick={(arg)=>{
+        console.log('%c%s','font-size:42pt;color:blue','app function')
+        console.log(arg)
+      }}
+
+
+       type='button' bgcolor="tomato" color="green" style={{width:'250px'}}>
+      <img src="https://cdn4.iconfinder.com/data/icons/evil-icons-user-interface/64/check-256.png" alt="ok" />
+      </Button>
       <Button  text="blaabbd" />
       <Button text="blaabbd" />
     </div>
--- src/App/component /ui/Button.jsx
+++ src/App/component /ui/Button.jsx
@@ -1,6 +1,32 @@
 import React from "react";
-const Button=(props)=>{
-    console.log(props)
-    return <button> {props.text} </button>
-}
+import style from "./Button.module.css";
+import PropTypes from "prop-types";
+const Button = (props) => {
+  console.log(props);
+  return (
+    <button
+      style={{
+        ...props.style,
+        backgroundColor: props.bgcolor,
+        color: props.color,
+      }}
+      type={props.type}
+      className={style.Button}
+    >
+      {" "}
+      {props.children}
+    </button>
+  );
+};
+//specification dans la console ce que l"on doit respecter dans l'app.jsx
+Button.propTypes = {
+  type: PropTypes.oneOf(["button", "reset", "submit"]),
+  bgcolor: PropTypes.oneOf(["tomato", "skyblue", "lime"]),
+  color: PropTypes.oneOf(["White", "black "]),
+  children: PropTypes.any.isRequired,
+  style: PropTypes.shape({
+    width: PropTypes.string,
+    height:PropTypes.string
+  }),
+};
 export default Button;
