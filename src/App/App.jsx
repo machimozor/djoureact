e47785643d8c053a05855790d4725ab9de712a60
machimozor <nabhaneibrahim77@gmail.com>
2023-06-19 12:35:40 +0200

    mise en forme , utilisation de props.propriete

---
+++ src/App/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './App.css';
+import Button from './component /ui/Button';
+
+const App = (props) =>
+{
+  
+  return (
+    <div className="App">
+      <Button text="blaabbd" />
+      <Button  text="blaabbd" />
+      <Button text="blaabbd" />
+    </div>
+  
+  );
+}
+
+export default App;
---
+++ src/App/component /ui/Button.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+const Button=(props)=>{
+    console.log(props)
+    return <button> {props.text} </button>
+}
+export default Button;
